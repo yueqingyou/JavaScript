1a0bc70e1982fd5764b73903bd53faddc8a844ea
yuesir <yueqingyou@gmail.com>
2020-12-24 17:27:40 +0800

    1

---
+++ JS Loan Calculator/js/index.js
@@ -0,0 +1,11 @@
+"use strict";
+//查找输出的元素
+function calculate() {
+  var amount = document.getElementById("amount");
+  var apr = document.getElementById("apr");
+  var years = document.getElementById("years");
+  var zipcode = document.getElementById("zipcode");
+  var payment = document.getElementById("payment");
+  var total = document.getElementById("total");
+  var totalinterest = document.getElementById("totalinterest");
+}
